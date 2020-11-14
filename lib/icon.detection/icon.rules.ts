dd43227447ec97fcfe07a5fe1a4059ac337e87b1
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-14 19:49:18 +0900

    adjusted icons rule checking

--- lib/icon.detection/icon.rules.ts
+++ lib/icon.detection/icon.rules.ts
@@ -1,5 +1,6 @@
 import { DetectionRule } from "../rules"
 import { DEFAULT_ICON_NAMING_CONVENTION_PATTERNS } from "@reflect.bridged.xyz/linter/lib/constants/naming-conventions/icon.naming"
+import { ReflectSceneNodeType } from "@bridged.xyz/design-sdk/lib/nodes"
 
 /**
  * material: https://material.io/design/iconography/system-icons.html
@@ -9,5 +10,46 @@ export default <DetectionRule>{
     minSize: 12,
     maxSize: 40,
     mustBeSquare: true,
+    allowedTypes: [
+        {
+            target: '*',
+            allow: false
+        },
+        {
+            target: ReflectSceneNodeType.frame,
+            allow: true
+        },
+        {
+            target: ReflectSceneNodeType.group,
+            allow: true
+        }, {
+            target: ReflectSceneNodeType.component,
+            allow: true
+        }, {
+            target: ReflectSceneNodeType.instance,
+            allow: true
+        }, {
+            target: ReflectSceneNodeType.vector,
+            allow: true
+        },
+    ],
+    allowedChildren: [
+        {
+            target: '*',
+            allow: true
+        },
+        {
+            target: ReflectSceneNodeType.text,
+            allow: false
+        },
+        {
+            target: ReflectSceneNodeType.image,
+            allow: false
+        },
+        {
+            target: ReflectSceneNodeType.instance,
+            allow: false
+        },
+    ],
     allowedNamePatterns: DEFAULT_ICON_NAMING_CONVENTION_PATTERNS
 }
--- lib/icon.detection/index.ts
+++ lib/icon.detection/index.ts
@@ -4,6 +4,7 @@ import { checkIfSquare } from "../processors/square.check";
 import { checkIfValidSize } from "../processors/size.check";
 import { checkIfValidName } from "../processors/name.check";
 import { ReflectSceneNode } from '@bridged.xyz/design-sdk/lib/nodes'
+import { checkIfValidStructure } from "../processors/structure.check";
 
 export function detectIfIcon(node: ReflectSceneNode): DetectionResult {
     const name = node.name;
@@ -16,12 +17,43 @@ export function detectIfIcon(node: ReflectSceneNode): DetectionResult {
         }
     }
 
+
     const sizeValidationResult: CheckResult = checkIfValidSize(node, rule)
+    if (!sizeValidationResult.result) {
+        return {
+            result: false,
+            entity: "icon",
+            accuracy: 1,
+            reason: ['size validation failed', ...sizeValidationResult.reason]
+        }
+    }
+
+
+
     const squareValidationResult: CheckResult = checkIfSquare(node)
-    const isIcon = sizeValidationResult.result && squareValidationResult.result
+    if (!squareValidationResult.result) {
+        return {
+            result: false,
+            entity: "icon",
+            accuracy: 1,
+            reason: ['not a square', ...squareValidationResult.reason]
+        }
+    }
+
+
+    const structureValidation = checkIfValidStructure(node, rule)
+    if (!structureValidation.result) {
+        return {
+            result: false,
+            entity: "icon",
+            accuracy: 1,
+            reason: ['structure validation fialed', ...structureValidation.reason]
+        }
+    }
+
     return {
-        result: isIcon,
-        entity: isIcon ? "icon" : "unknown",
-        accuracy: 0.1
+        result: true,
+        entity: "icon",
+        accuracy: 0.5
     }
 }
