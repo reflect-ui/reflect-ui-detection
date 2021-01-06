28eef506dcc27b2f875449485a06ca9ec0415303
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-06 20:53:32 +0900

    fixed name detection safe logic for minimatch glob pattern usage

--- lib/processors/name.check.ts
+++ lib/processors/name.check.ts
@@ -7,18 +7,24 @@ import minimatch from "minimatch"
  * @param rule 
  */
 export function checkIfValidName(name: string, rule: NamingRule): boolean {
-    // return true by default if no allowed naming convention rule provided.
-    if (rule.allowedNamePatterns === undefined) {
-        return true
-    }
-
-    for (const pattern of rule.allowedNamePatterns) {
-        // pattern matching with ignoring case.
-        const matched = minimatch(name, pattern, { nocase: true })
-        if (matched) {
-            return true;
+    try {
+        // return true by default if no allowed naming convention rule provided.
+        if (rule.allowedNamePatterns === undefined) {
+            return true
         }
-    }
 
-    return false
+        for (const pattern of rule.allowedNamePatterns) {
+            // pattern matching with ignoring case.
+            const matched = minimatch(name, pattern, { nocase: true })
+            if (matched) {
+                return true;
+            }
+        }
+        return false    
+
+    } catch (e) {
+        console.error(e)
+        console.log(`name validation returning false, due to invalid rule input or name input. (for name ${name} and rule ${rule})`)
+        return false
+    }
 }
