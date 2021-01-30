06a366dcadc57b76b1f6d06b875ff31701ec2088
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-31 04:11:37 +0900

    using icon lint constants based on reflect core's definiton

--- lib/icon.detection/icon.rules.ts
+++ lib/icon.detection/icon.rules.ts
@@ -1,57 +1,63 @@
-import { DetectionRule } from "../rules"
-import { DEFAULT_ICON_NAMING_CONVENTION_PATTERNS } from "@reflect.bridged.xyz/linter/lib/constants/naming-conventions/icon.naming"
-import { ReflectSceneNodeType } from "@bridged.xyz/design-sdk/lib/nodes"
-
+import { DetectionRule } from "../rules";
+import { DEFAULT_ICON_NAMING_CONVENTION_PATTERNS } from "@reflect.bridged.xyz/linter/lib/constants/naming-conventions/icon.naming";
+import { ReflectSceneNodeType } from "@bridged.xyz/design-sdk/lib/nodes";
+import {
+  ICON_MAX_SIZE,
+  ICON_MIN_SIZE,
+} from "@reflect.bridged.xyz/core/lib/icon/icon.constants";
 /**
  * material: https://material.io/design/iconography/system-icons.html
  * reflect: todo
  */
 export default <DetectionRule>{
-    minSize: 12,
-    maxSize: 40,
-    minRatio: 1,
-    maxRatio: 1.5,
-    mustBeSquare: true,
-    allowedTypes: [
-        {
-            target: '*',
-            allow: false
-        },
-        {
-            target: ReflectSceneNodeType.frame,
-            allow: true
-        },
-        {
-            target: ReflectSceneNodeType.group,
-            allow: true
-        }, {
-            target: ReflectSceneNodeType.component,
-            allow: true
-        }, {
-            target: ReflectSceneNodeType.instance,
-            allow: true
-        }, {
-            target: ReflectSceneNodeType.vector,
-            allow: true
-        },
-    ],
-    allowedChildren: [
-        {
-            target: '*',
-            allow: true
-        },
-        {
-            target: ReflectSceneNodeType.text,
-            allow: false
-        },
-        {
-            target: ReflectSceneNodeType.image,
-            allow: false
-        },
-        {
-            target: ReflectSceneNodeType.instance,
-            allow: false
-        },
-    ],
-    allowedNamePatterns: DEFAULT_ICON_NAMING_CONVENTION_PATTERNS
-}
+  minSize: ICON_MIN_SIZE,
+  maxSize: ICON_MAX_SIZE,
+  minRatio: 1,
+  maxRatio: 1.5,
+  mustBeSquare: true,
+  allowedTypes: [
+    {
+      target: "*",
+      allow: false,
+    },
+    {
+      target: ReflectSceneNodeType.frame,
+      allow: true,
+    },
+    {
+      target: ReflectSceneNodeType.group,
+      allow: true,
+    },
+    {
+      target: ReflectSceneNodeType.component,
+      allow: true,
+    },
+    {
+      target: ReflectSceneNodeType.instance,
+      allow: true,
+    },
+    {
+      target: ReflectSceneNodeType.vector,
+      allow: true,
+    },
+  ],
+  allowedChildren: [
+    {
+      target: "*",
+      allow: true,
+    },
+    {
+      target: ReflectSceneNodeType.text,
+      allow: false,
+    },
+    {
+      target: ReflectSceneNodeType.image,
+      allow: false,
+    },
+    {
+      target: ReflectSceneNodeType.instance,
+      allow: false,
+    },
+  ],
+  allowedNamePatterns: DEFAULT_ICON_NAMING_CONVENTION_PATTERNS,
+};
